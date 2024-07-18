48c59e8eb97adbbeb869cddc10d239d375d3789f
Adhiraj Bharadwaj <142144938+AdhirajBharadwaj@users.noreply.github.com>
2024-07-18 08:36:24 +0530

    Add files via upload

---
+++ reactquiz/src/components/Options.js
@@ -0,0 +1,17 @@
+ 
+
+export default function Options({question,dispatch,answer}) {
+    const hasA=answer!==null;
+    const options=question.options;
+  return (
+    <div className="options">
+        {options.map((option,index)=>
+            <button className={`btn btn-option ${index===answer?"answer": ""} ${hasA?index===question.correctOption?"correct":"wrong":""}`} key={option} disabled={hasA} onClick={()=>dispatch({
+                type:"newAnswer",payload:index
+            })}>{option}</button>
+        )}
+
+    </div>
+    
+  )
+}
