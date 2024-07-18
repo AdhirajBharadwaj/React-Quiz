03cc75697d65e37c670cc6591aeba5c4fee07be5
Adhiraj Bharadwaj <adhirajbharadwaj30@gmail.com>
2024-07-18 08:36:55 +0530

    new

---
+++ reactquiz/src/components/StartScreen.js
@@ -0,0 +1,11 @@
+
+
+export default function StartScreen({num,dispatch}) {
+  return (
+    <div className="start">
+        <h2>Welcome to The React Quiz!</h2>
+        <h3>{num} questions to test your React mastery</h3>
+        <button onClick={()=>dispatch({type:"start"})}>Lets start</button>
+    </div>
+  )
+}
